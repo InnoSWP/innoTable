830d62a2d97c224b5b16feb18e52fda2ec3f59f0
dmfrpro <dmfr2021y@gmail.com>
2022-06-18 16:15:34 +0300

    Move Controller.java into controller package * Fix minor package tree mistake
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

---
+++ src/main/java/org/innoswp/innotable/controller/Controller.java
@@ -0,0 +1,5 @@
+package org.innoswp.innotable.controller;
+
+public interface Controller {
+    void run();
+}
--- src/main/java/org/innoswp/innotable/controller/forwarder/Controller.java
+++
@@ -1,5 +0,0 @@
-package org.innoswp.innotable.controller.forwarder;
-
-public interface Controller {
-    void run();
-}
