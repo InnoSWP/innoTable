2bc6920d2eaf22fedb9e6859203fd4a373dd1128
dmfrpro <dmfr2021y@gmail.com>
2022-06-18 18:59:05 +0300

    Update User.java: add equals and hashCode
    * Required for correct comparison of Users
    
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- src/main/java/org/innoswp/innotable/model/user/User.java
+++ src/main/java/org/innoswp/innotable/model/user/User.java
@@ -1,10 +1,12 @@
 package org.innoswp.innotable.model.user;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public abstract class User {
     private String email;
     private String password;
