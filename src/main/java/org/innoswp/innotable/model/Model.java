cd71113fc0b888b09de7b94282644cdeeb9edc46
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-06-22 00:01:01 +0300

    Update Model.java: introduce unified methods
    * Added throws Exception for each method
    * Added new methods
    
    Signed-off-by: Kirill Batyshchev <k.batyshchev@innopolis.university>

--- src/main/java/org/innoswp/innotable/model/Model.java
+++ src/main/java/org/innoswp/innotable/model/Model.java
@@ -5,27 +5,48 @@ import org.innoswp.innotable.model.user.Group;
 import org.innoswp.innotable.model.user.Role;
 import org.innoswp.innotable.model.user.User;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface Model {
+    List<Group> getGroups() throws Exception;
 
-    List<Group> getGroups();
+    void saveGroup(Group group) throws Exception;
 
-    void saveGroup(Group group);
+    Group dropGroup(Group group) throws Exception;
 
-    List<Role> getRoles();
+    List<Role> getRoles() throws Exception;
 
-    void saveRole(Role role);
+    void saveRole(Role role) throws Exception;
 
-    void saveEvent(CalendarEvent calendarEvent, Group group);
+    void saveEvent(CalendarEvent calendarEvent, Group group) throws Exception;
 
-    Pair<User, List<CalendarEvent>> loadEventsByUser(User user);
+    Pair<Group, CalendarEvent> dropEvent(String title) throws Exception;
 
-    List<Pair<Group, CalendarEvent>> loadAllEvents();
+    List<Pair<Group, CalendarEvent>> loadEventsByUser(User user) throws Exception;
 
-    List<User> getUsers();
+    List<Pair<Group, CalendarEvent>> loadEventsByGroup(Group group) throws Exception;
 
-    List<User> getUsersByGroup(Group group);
+    List<Pair<Group, CalendarEvent>> loadEventsByDay(LocalDate date) throws Exception;
 
-    List<User> getUsersByRole(Role role);
+    List<Pair<Group, CalendarEvent>> loadEventsIn(LocalDateTime start, LocalDateTime end) throws Exception;
+
+    List<Pair<Group, CalendarEvent>> loadEventsDuring(LocalDateTime time) throws Exception;
+
+    List<Pair<Group, CalendarEvent>> loadEventsByTitle(String title) throws Exception;
+
+    List<Pair<Group, CalendarEvent>> loadAllEvents() throws Exception;
+
+    void addUser(User user) throws Exception;
+
+    void assignUsersToGroup(List<User> users, Group group) throws Exception;
+
+    User dropUser(String email) throws Exception;
+
+    List<User> getUsers() throws Exception;
+
+    List<User> getUsersByGroup(Group group) throws Exception;
+
+    List<User> getUsersByRole(Role role) throws Exception;
 }
