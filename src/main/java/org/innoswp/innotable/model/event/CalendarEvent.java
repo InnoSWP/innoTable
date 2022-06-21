a5990d22eb640d65f88adac5f8b972304ca3d5c0
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-06-22 00:02:36 +0300

    Update CalendarEvent.java: Convert to a record
    Signed-off-by: Kirill Batyshchev <k.batyshchev@innopolis.university>

--- src/main/java/org/innoswp/innotable/model/event/CalendarEvent.java
+++ src/main/java/org/innoswp/innotable/model/event/CalendarEvent.java
@@ -1,15 +1,7 @@
 package org.innoswp.innotable.model.event;
 
-import lombok.Data;
-
 import java.time.LocalDateTime;
 
-@Data
-public abstract class CalendarEvent {
-    private final long id;
-    private String title;
-    private String description;
-    private Location location;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+public record CalendarEvent(String title, String description, Location location, LocalDateTime startTime,
+                            LocalDateTime endTime) {
 }
