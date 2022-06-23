23b9cd0ce6ca5d0b40c7584ca47367f56cf2a278
dmfrpro <dmfr2021y@gmail.com>
2022-06-23 06:51:43 +0300

    Update CalendarEvent.java: add groups
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- src/main/java/org/innoswp/innotable/model/event/CalendarEvent.java
+++ src/main/java/org/innoswp/innotable/model/event/CalendarEvent.java
@@ -1,7 +1,10 @@
 package org.innoswp.innotable.model.event;
 
+import org.innoswp.innotable.model.user.Group;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record CalendarEvent(String title, String description, Location location, LocalDateTime startTime,
-                            LocalDateTime endTime) {
+                            LocalDateTime endTime, List<Group> groups) {
 }
