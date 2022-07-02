7476660219e1cb183a1973268b742037f8dc419f
dmfrpro <dmfr2021y@gmail.com>
2022-07-02 19:24:17 +0300

    Refactor: fix database connection and initialization
    * By adding springboot-starter-jdbc module the process of database communication is successful
    * Fix adding new mock data: ignore if it exists
    
    Signed-off-by: dmfrpro <dmfr2021y@gmail.com>

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,24 +1,28 @@
--- ЕTHis file contains only MOCK data which will be filled into the schema
+-- This file contains only MOCK data which will be filled into the schema
 
-INSERT INTO role
+INSERT INTO role(label)
 VALUES ('admin'),
-       ('user');
+       ('user')
+ON CONFLICT DO NOTHING;
 
-INSERT INTO "group"
+INSERT INTO "group"(label)
 VALUES ('bachelor'),
        ('master'),
-       ('phd');
+       ('phd')
+ON CONFLICT DO NOTHING;
 
-INSERT INTO "user"
+INSERT INTO "user"(email, password, role_id)
 VALUES ('d.alekhin@innopolis.university', '12345', 1),
-       ('g.budnik@innopolis.university', '12345', 1),
+       ('g.budnik@innopolis.university', '12345', 2),
        ('k.batyshchev@innopolis.university', '12345', 2),
        ('a.palashkina@innopolis.university', '12345', 2),
-       ('guz.zakirova@innopolis.university', '12345', 2);
+       ('guz.zakirova@innopolis.university', '12345', 2)
+ON CONFLICT DO NOTHING;
 
-INSERT INTO "user_group"
+INSERT INTO "user_group"(user_id, group_id)
 VALUES (1, 1),
        (2, 1),
        (3, 2),
        (4, 3),
-       (5, 4);
+       (5, 3)
+ON CONFLICT DO NOTHING;
