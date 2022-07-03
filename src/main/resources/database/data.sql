244da576dacacc5496b60cd47df1a65deb4cc74e
BatyshchevKirill <k.batyshchev@innopolis.university>
2022-07-03 17:19:19 +0300

    Fix populating data
    Signed-off-by: Kirill Batyshchev <k.batyshchev@innopolis.university>

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,4 +1,8 @@
 -- This file contains only MOCK data which will be filled into the schema
+DELETE FROM "user";
+DELETE FROM "group";
+DELETE FROM role;
+DELETE FROM event;
 
 INSERT INTO role(label)
 VALUES ('admin'),
@@ -12,17 +16,17 @@ VALUES ('bachelor'),
 ON CONFLICT DO NOTHING;
 
 INSERT INTO "user"(email, password, role_id)
-VALUES ('d.alekhin@innopolis.university', '12345', 1),
-       ('g.budnik@innopolis.university', '12345', 2),
-       ('k.batyshchev@innopolis.university', '12345', 2),
-       ('a.palashkina@innopolis.university', '12345', 2),
-       ('guz.zakirova@innopolis.university', '12345', 2)
+VALUES ('d.alekhin@innopolis.university', '12345', (SELECT id FROM role WHERE label = 'admin')),
+       ('g.budnik@innopolis.university', '12345', (SELECT id FROM role WHERE label = 'user')),
+       ('k.batyshchev@innopolis.university', '12345', (SELECT id FROM role WHERE label = 'user')),
+       ('a.palashkina@innopolis.university', '12345', (SELECT id FROM role WHERE label = 'user')),
+       ('guz.zakirova@innopolis.university', '12345', (SELECT id FROM role WHERE label = 'user'))
 ON CONFLICT DO NOTHING;
 
 INSERT INTO "user_group"(user_id, group_id)
-VALUES (1, 1),
-       (2, 1),
-       (3, 2),
-       (4, 3),
-       (5, 3)
+VALUES ((SELECT id FROM "user" WHERE email LIKE 'd.al%'), (SELECT id FROM "group" WHERE label = 'bachelor')),
+       ((SELECT id FROM "user" WHERE email LIKE 'g.bud%'), (SELECT id FROM "group" WHERE label = 'bachelor')),
+       ((SELECT id FROM "user" WHERE email LIKE 'k.bat%'), (SELECT id FROM "group" WHERE label = 'master')),
+       ((SELECT id FROM "user" WHERE email LIKE 'a.pal%'), (SELECT id FROM "group" WHERE label = 'phd')),
+       ((SELECT id FROM "user" WHERE email LIKE 'guz.z%'), (SELECT id FROM "group" WHERE label = 'phd'))
 ON CONFLICT DO NOTHING;
